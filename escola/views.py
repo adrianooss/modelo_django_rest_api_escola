daa428e4019e8882834f7909a4c1fe8ac630e418
unknown <adrianooss@live.com>
2025-10-17 19:53:20 -0300

    Adiciona filtros e ordenação nos viewsets, atualiza admin para busca por CPF, e implementa scripts para popular banco de dados com cursos e estudantes

--- escola/views.py
+++ escola/views.py
@@ -2,39 +2,31 @@ from escola.models import Estudante, Curso, Matricula
 from escola.serializers import \
     EstudanteSerializer,CursoSerializer, MatriculaSerializer, \
     ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
-from rest_framework import viewsets, generics
-# from rest_framework.authentication import BasicAuthentication
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class EstudanteViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
 
 class CursoViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 class ListaMatriculaEstudante(generics.ListAPIView):
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
         return queryset
--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "escola",
     "rest_framework",
+    "django_filters"
 ]
 
 MIDDLEWARE = [
@@ -129,5 +130,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
