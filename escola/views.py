fc0d9d2f7e93ea121f22b843fef65313b19fe354
unknown <adrianooss@live.com>
2025-11-03 21:31:27 -0300

    Criação de documentação da API via Swagger e Redoc

--- escola/views.py
+++ escola/views.py
@@ -8,6 +8,24 @@ from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from escola.throttles import MatriculaAnonRateThrottle
 
 class EstudanteViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da View:
+    - Endpoint para CRUD de estudantes.
+    
+    Campos de ordenação:
+    -nome : permite ordenar os resultados por nome.
+    
+    Campos de busca:
+    -nome : permite buscar estudantes pelo nome.
+    -cpf : permite buscar estudantes pelo CPF.
+    
+    Métodos HTTP permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+    
+    Classe de Serializer:
+    - EstudanteSerializer: usado para serializar e desserializar dados de estudantes.
+    - Se API for 'v2', EstudanteSerializerV2 será usado.
+    """
     queryset = Estudante.objects.all().order_by("id")
     #serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend,filters.OrderingFilter, filters.SearchFilter]
@@ -19,25 +37,51 @@ class EstudanteViewSet(viewsets.ModelViewSet):
         return EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da View:
+    - Endpoint para CRUD de cursos.
+
+    Métodos HTTP permitidos:
+    - GET, POST
+    Throttle: Classes:
+    - UserRateThrottle: Limita a taxa de requisições por usuário autenticado.
+    - MatriculaAnonRateThrottle: Limita a taxa de requisições por usuários anônimos.
+    """
     queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da View:
+    - ViewSet de Matrículas
+    Parâmetros:
+    - queryset: Retorna todas as matrículas ordenadas pelo campo id
+    """
     queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
     throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle]
     http_method_names = ["get","post"]
 
 class ListaMatriculaEstudante(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Estudante
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Curso
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasCursoSerializer
-
-
--- setup/urls.py
+++ setup/urls.py
@@ -3,6 +3,20 @@ from django.urls import path, include
 from escola.views import \
     EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculaCurso
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação da API",
+      default_version='v1',
+      description="Documentação da API Escola Saber Aprender",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet, basename = "Estudantes")
@@ -14,4 +28,7 @@ urlpatterns = [
     path('',include(router.urls)),
     path('estudantes/<int:pk>/matriculas/',ListaMatriculaEstudante.as_view()),
     path('cursos/<int:pk>/matriculas/',ListaMatriculaCurso.as_view()),
+    path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
